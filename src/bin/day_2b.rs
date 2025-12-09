7df41af52d3d2a0a1bb4a75aaff807bd349f50d3
Mister Teddy <hongphat.js@gmail.com>
2025-12-09 23:49:56 +0100

    day 2

---
+++ src/bin/day_2b.rs
@@ -0,0 +1,44 @@
+use advent_of_code_2025::get_input;
+
+fn main() {
+    let input = get_input();
+
+    fn is_invalid(x: usize) -> bool {
+        let s = x.to_string();
+        // actually, it is useful for task 2
+        'a: for size in 1..=s.len() / 2 {
+            // Split s into chunks of length len
+            let chunks: Vec<&[u8]> = s.as_bytes().chunks(size).collect();
+            // Check if all chunks are the same
+            for chunk in &chunks {
+                if chunk != &chunks[0] {
+                    continue 'a;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    let mut res = 0;
+    for line in input.split(",") {
+        let (start, end) = line
+            .split_once("-")
+            .map(|(start, end)| {
+                (
+                    start.parse::<usize>().unwrap(),
+                    end.parse::<usize>().unwrap(),
+                )
+            })
+            .unwrap();
+        res += (start..=end)
+            .filter(|&x| is_invalid(x))
+            .map(|x| {
+                println!("❌ {x}");
+                x
+            })
+            .sum::<usize>();
+    }
+
+    println!("👉 {res}");
+}
